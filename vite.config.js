8f1b0fb7dbfa5b89b03df78dcbb5adc67b5fa753
SYED TAWSEEF <syed tawseef>
2024-06-03 09:18:48 +0530

    updated vite cofig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-Fast-Pizza/", // Set this to your repository name
   plugins: [react()],
-})
+});
